ee15152dd548eb4d974ef67186c07da62c3ec997
Bruno Gola <bgola@Fabs.(none)>
2009-01-26 22:00:44 -0200

    changes to tests  and adding subscriptions

--- test/unit_tests.py
+++ test/unit_tests.py
@@ -11,3 +11,22 @@ class EventTest(unittest.TestCase):
 
   def testShouldPass(self):
     pass
+    
+class SubscriptionTest(unittest.TestCase):
+
+  def setUp(self):
+    self.subscription = model.Subscription()
+
+  def tearDown(self):
+    pass
+
+  def testShouldPass(self):
+    pass
+    
+  def testShouldCreateSubscription(self):
+    self.subscription.name = "Teste"
+    self.subscription.email = "teste@gmail.com"
+    event = model.Event()
+    event.put()
+    self.subscription.event = event
+    self.subscription.put()
