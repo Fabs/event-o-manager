4d083c38bb3755af5763c451a7a40e6d196f7753
Vanessa <bani@one-bani.(none)>
2009-01-26 22:26:06 -0200

    teste get_all_subscriptions

--- model.py
+++ model.py
@@ -5,6 +5,10 @@ class Event(db.Model):
     description = db.StringProperty(multiline=True)
     slots = db.IntegerProperty()
     
+    def get_all_subscriptions(self):
+        subscriptions = Subscription.gql("WHERE event = :1",self.key()).fetch(1000)
+        return subscriptions
+    
 class Subscription(db.Model):
     name = db.StringProperty()
     email = db.EmailProperty()
--- test/unit_tests.py
+++ test/unit_tests.py
@@ -12,6 +12,15 @@ class EventTest(unittest.TestCase):
   def testShouldPass(self):
     pass
     
+  def testGetSubscriptions(self):
+    subscription = model.Subscription()
+    subscription.name = "Teste"
+    subscription.email = "teste@gmail.com"
+    self.event.put()
+    subscription.event = self.event
+    subscription.put()
+    assert subscription.key() == self.event.get_all_subscriptions()[0].key()
+    
 class SubscriptionTest(unittest.TestCase):
 
   def setUp(self):
