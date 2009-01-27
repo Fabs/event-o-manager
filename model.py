256e14a0c5a1472eaafef0f78a3fa29715a6fd3f
Vanessa <bani@one-bani.(none)>
2009-01-26 23:16:29 -0200

    modelos não testados

--- model.py
+++ model.py
@@ -4,14 +4,23 @@ class Event(db.Model):
     name = db.StringProperty()
     description = db.StringProperty(multiline=True)
     slots = db.IntegerProperty()
+    date = db.DateTimeProperty(auto_now_add=True)
     
     def get_all_subscriptions(self):
         subscriptions = Subscription.gql("WHERE event = :1",self.key()).fetch(1000)
         return subscriptions
         
+    def get_all_enrolled(self):
+        subscriptions = Subscription.gql("WHERE event = :1 ORDER date",self.key())fetch(self.slots)
+        return subscriptions
+        
+    def get_all_waiting(self):
+        subscriptions = Subscription.gql("WHERE event = :1 ORDER date",self.key()).fetch(1000, self.slots)
+        return subscriptions
+        
     def add_subscription(self, subscription):
-        if self.slots <= self.count_subscriptions():
-            raise SlotLimitException, "No slot for you!"
+        if Subscription.gql("WHERE event = :1 and email = :2", self.key(), subscription.email).count() > 0:
+            raise AlreadySubscribedException, "This e-mail is already subscribed. If you didn't subscribe, wait for the validation to expire"
         subscription.event = self
         subscription.put()
         
@@ -27,7 +36,7 @@ class Subscription(db.Model):
     event = db.ReferenceProperty(Event)
     
     
-class SlotLimitException(Exception):
+class AlreadySubscribedException(Exception):
     pass
     
 
